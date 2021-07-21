c1ef2f1f80d0aec0487b02f7e7b4160be672dcfc
Amit Nino <amitnino@moveo.co.il>
2021-07-21 11:53:39 +0300

    fixed-logic

--- src/components/nav-links/NavLinks.tsx
+++ src/components/nav-links/NavLinks.tsx
@@ -6,11 +6,12 @@ type navLinksProps = {
 }
 
 const NavLinks: React.FC<navLinksProps> = ({navItems}: navLinksProps) => {
+
   return (
     <ul>
       {navItems.map((navItem) => (
         <li>
-          <a href={navItem.route}>{navItem.name && navItem.icon && navItem.route}</a>
+          <a href={navItem.route}>{navItem.name ? navItem.name : (navItem.route && navItem.icon)}</a>
         </li>
       ))}
     </ul>
