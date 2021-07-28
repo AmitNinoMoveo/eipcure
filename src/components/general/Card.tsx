4c3af9fcf7b5aa88732002da8e7b174f7a32606d
Amit Nino <amitnino@moveo.co.il>
2021-07-28 12:05:01 +0300

    Created card types and finished logic | added general styles | adding carousel to chef-week

--- src/components/general/Card.tsx
+++ src/components/general/Card.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import "../../assets/styles/components/card.scss";
+import React, { FC } from "react";
+import CardProps from "../../utils/interfaces/card";
 
-const CardComponent = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const CardComponent: FC<CardProps> = (props: CardProps) => {
+  const MediumCard = () => <h3>{props.type === "medium" && props.subTitle}</h3>;
+  const LargeCard = () => (
+    <>
+      <p id="discription">{props.type === "large" && props.discription}</p>
+      <img
+        src={props.type === "large" ? props.icon : ""}
+        alt="icon"
+        id="icon"
+      />
+      <div className="price-container">
+        <div className="line" />
+        <p>&#8362;{props.type === "large" && props.price}</p>
+        <div className="line" />
+      </div>
+    </>
+  );
+
+  return (
+    <div
+      id={props.id.toString()}
+      className={`card-container ${props.type} ${
+        props.carousel && "carousel-card"
+      }`}
+    >
+      <img src={props.picture} alt="props.title" />
+      <h2>{props.title}</h2>
+      {props.type === "medium" && <MediumCard />}
+      {props.type === "large" && <LargeCard />}
+    </div>
+  );
+};
 
 export default CardComponent;
--- src/components/home-page/chefOfTheWeek.tsx
+++ src/components/home-page/chefOfTheWeek.tsx
@@ -4,9 +4,13 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../state/store";
 import CarouselComponent from "../general/Carousel";
 import CardComponent from "../general/Card";
+import { useAppSelector } from "../../state/hooks";
 
 const ChefOfTheWeekComponent = () => {
   const chef = useSelector((state: RootState) => state.data.chefWeek);
+  const displaySize = useAppSelector(
+    (state: RootState) => state.generalUISettings.displaySize
+  );
 
   const ChefPortrait = () => (
     <div
@@ -20,18 +24,25 @@ const ChefOfTheWeekComponent = () => {
   const Carousel = () => (
     <CarouselComponent>
       {chef.restaurants.map((restaurant) => (
-        <CardComponent key={restaurant.id} />
+        <CardComponent
+          key={restaurant.id}
+          id={restaurant.id}
+          type="small"
+          title={restaurant.name}
+          carousel={true}
+          picture={restaurant.picture}
+        />
       ))}
     </CarouselComponent>
   );
 
   return (
     <section className="chef-week-container">
-      <h3 className="text-uppercase" >chef of the week:</h3>
+      <h3 className="text-uppercase">chef of the week:</h3>
       <ChefPortrait />
       <p>{chef.description}</p>
-      <h3>{chef.chefName}'s restaurants:</h3>
-      <Carousel />
+      <h3 className="self-start" >{chef.chefName}'s restaurants:</h3>
+      {displaySize !== "web" ? <Carousel /> : <></>}
     </section>
   );
 };
